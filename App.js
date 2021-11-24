84260a8e403921891200ff699be64fb9f3cc797f
Your Name <you@example.com>
2021-11-24 13:05:01 +0700

    update

--- App.js
+++ App.js
@@ -19,6 +19,12 @@ const Header = (props) => {
             >
                 React Native Tutorial
             </Text>
+
+            <Text
+                style={styles.text}
+            >
+                ............
+            </Text>
         </View>
     )
 }
