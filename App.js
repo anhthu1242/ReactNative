bcec8378a1ef171f8a1b2d01f9ef2c27f65bad33
Your Name <you@example.com>
2021-11-24 13:01:53 +0700

    update

---
+++ App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+} from 'react-native';
+
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+const Header = (props) => {
+    return (
+        <View
+            style={styles.view}
+        >
+            <Text
+                style={styles.text}
+            >
+                React Native Tutorial
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    view: {
+        width:'100%',
+        height: 50,
+        backgroundColor: '#00f',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text:{
+        fontSize: 25,
+        fontWeight:'bold',
+        color:'#ffffff'
+    }
+})
+
+export default Header;
