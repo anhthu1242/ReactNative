23b43715460c1734ecfe3d6811f8620bbb481da1
anhthunguyen <59395046+anhthu1242@users.noreply.github.com>
2021-11-27 09:38:20 +0700

    Update App.js

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { createMaterialBottomTabNavigator } from '@react-navigation/material-bot
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import ScreenA from './ScreenA';
 import ScreenB from './ScreenB';
+import ScreenC from './ScreenC';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 // const Tab = createBottomTabNavigator();
